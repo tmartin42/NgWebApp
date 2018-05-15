0024b4ebadf4b9ca5b4f406286599d6cbeb892e4
Thibault MARTIN <tmartin@e3r4p7.42.fr>
2018-05-15 23:07:14 +0200

    added error system

---
+++ src/app/error.service.ts
@@ -0,0 +1,33 @@
+import { Component, Injectable, Input, Output, EventEmitter } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Jsonp } from '@angular/http';
+
+@Injectable()
+export class DataService {
+
+  constructor(private http: HttpClient, private jsonp: Jsonp) {}
+
+  getUsers(): Observable<any> {
+    return this.http.get(`http://localhost:3000/playlists` );
+  }
+
+  getData() {
+    return this.http.get('http://localhost:3000/data');
+  }
+
+  getTrack(id) {
+    return this.jsonp.request(`https://api.deezer.com/track/${id}?output=jsonp&callback=JSONP_CALLBACK`, {method: 'Get'})
+      .map(res => {
+        return res.json();
+      });
+  }
+
+  reallisten(url) {
+
+    return this.jsonp.request(`https://api.deezer.com/oembed?url=${url}&output=jsonp&callback=JSONP_CALLBACK`, {method: 'Get'})
+      .map(res => {
+        return res.json();
+      });
+  }
+}
